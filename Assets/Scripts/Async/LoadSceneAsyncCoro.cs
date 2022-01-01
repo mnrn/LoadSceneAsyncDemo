4a5fe626140efdace7cca60cbdb8f24b98ef965b
mnrn <paper.airplane01@gmail.com>
2022-01-01 16:17:31 +0900

    first commit

---
+++ Assets/Scripts/Async/LoadSceneAsyncCoro.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Async
+{
+    public class LoadSceneAsyncCoro : MonoBehaviour
+    {
+        [SerializeField] private GameObject canvas;
+        [SerializeField] private Slider slider;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void LoadSceneCoro(string scene)
+        {
+            StartCoroutine(LoadSceneCoroExec(scene));
+        }
+
+        IEnumerator LoadSceneCoroExec(string scene)
+        {
+            slider.value = 0.0f;
+            canvas.SetActive(true);
+            yield return new WaitForSeconds(1f);
+
+            // ロードを開始します
+            var asyncOp = SceneManager.LoadSceneAsync(scene);
+
+            // OKするまでシーンをアクティブにしません
+            asyncOp.allowSceneActivation = false;
+            Debug.Log("Progress :" + asyncOp.progress);
+
+            while (true)
+            {
+                yield return null;
+                slider.value = asyncOp.progress;
+                Debug.Log("Progress :" + asyncOp.progress);
+
+                if (asyncOp.progress >= 0.9f)
+                {
+                    slider.value = 1.0f;
+                    break;
+                }
+            }
+
+            yield return new WaitForSeconds(1f);
+            asyncOp.allowSceneActivation = true;
+            canvas.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Title/GameStartButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Scenes.Title
+{
+    public class GameStartButton : MonoBehaviour
+    {
+        [SerializeField] private GameObject loadSceneAsync;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnClicked()
+        {
+            loadSceneAsync.GetComponent<Async.LoadSceneAsyncCoro>()
+                .LoadSceneCoro("SceneGameMain");
+        }
+    }
+}
